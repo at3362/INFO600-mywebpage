009c07c947d11a30f8735b0a1b4f96afdc7e784f
at3362 <56407577+at3362@users.noreply.github.com>
2019-10-17 14:51:48 -0400

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', (event) => ){
 	console.log('DOM fully loaded and parsed');
 };
 
-const btTest = document.querySelector("#btTest");
+const btTest = document.querySelector('#btTest');
 
 		btTest.addEventListener('click', event => {
 		  btTest.style.backgroundColor = "red";
